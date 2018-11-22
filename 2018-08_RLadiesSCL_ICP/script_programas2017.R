28f54ed5f1374af3980e67c21b677f2c0dbb7a5c
Riva Quiroga <riva@rladies.org>
2018-11-22 15:25:02 -0300

    indicaciones previas

--- 2018-08_RLadiesSCL_ICP/script_programas2017.R
+++ 2018-08_RLadiesSCL_ICP/script_programas2017.R
@@ -2,13 +2,14 @@
 # Sesión 1: Análisis de discursos políticos Latinoamericanos
 # Riva Quroga (@rivaquiroga)
 
-
+# NOTA: Lo que haremos en esta sesión supone un manejo básico de R y RStudio. Puedes revisar el siguiente tutorial introductorio si aún no están tan familiarizada con este lenguaje de programación: http://bit.ly/tutorial-intro-R. En él se aborda el proceso de instalación y algunas funciones básicas. 
 
 #Si no los tienes ya, instala los siguientes paquetes:
 # install.packages("readtext")
 # install.packages("quanteda")
 # install.packages("tidyverse") # o al menos `dplyr`, `ggplot2` y `readr`
 
+# 1. Cargar los paquetes
 
 library(readtext) # para importar los archivos de texto
 library(quanteda) # para hacer el análisis
@@ -17,24 +18,40 @@ library(ggplot2) # para editar los gráficos que por defecto entrega quanteda
 library(readr) # para exportar las búsquedas de concordancias
 # También puedes cargar todo el Tidyverse: library(tidyverse)
 
-# Versión rápida para importar los textos:
+
+# 2. Crear un proyecto
+
+# Para hacer el trabajo más eficiente, trabajaremos en un "proyecto" de RStudio. Para eso, debes ir a File > New Project > Existing Directory, y eliges la carpeta en que guardaste los materiales de esta sesión.
+
+# 3. Importar los archivos
+
+# Con el proyecto listo, ya podemos importar los textos con los que trabajaremos. Para ello, crearemos un objeto llamado `programa` al que le asignaremos como valor todos los textos que están en la subcarpeta `programas_elecciones2017`:
+
 programas <- readtext("programas_elecciones2017/*")
 
-# Pero mejor usar más argumentos
+# Miremos el dataframe que se creó
+programas
+
+# Si bien eso es suficiente para importar los archivos, la función readtext() tiene varios argumentos que pueden hacer el proceso de crear un corpus más efectivo y eficiente. Por ejemplo, nos permite crear variables a partir de información contenida en el nombre del archivo, es decir, de los metadatos de los textos que vamos a analizar. Además, podemos indicar información relativa a la codificación de los textos:
 
 programas <- readtext("programas_elecciones2017/*",
                                      docvarsfrom = "filenames", 
                                      docvarnames = c("candidato", "eleccion", "coalicion"),   
                                      dvsep = "_",           
-                                     encoding = "UTF-8") 
+                                     encoding = "UTF-8") # ¡siempre UTF-8!
 
-# Creamos el corpus, con la función corpus(). Tenemos que indicar en qué columna está el texto.
+# Si mirarmos nuevamente el dataframe que creamos, ahora tenemos nuevas variables. Este es solo un ejemplo de lo importante que es utilizar sabiamente el nombre de los archivos. 
+
+programas
+
+# Para poder empezar el proceso de análisis con el paquete `quanteda`, necesitamos crear un tipo de objeto especial en R: un corpus. Para ello, utilizaremos la corpus(). Tenemos que indicar el objeto/dataframe que tiene nuestros datos y el nombre de la columna que contiene el texto:
+
 corpus_programas <- corpus(programas, text_field = "text")  
 
-# Damos una mirada al corpus
+# Demos una mirada al corpus
 summary(corpus_programas)
 
-# Sería mejor cambiar los nombres de los documentos (columna "Text"). Piensen que mientras más variables incluyan en el nombre, más largos serán. Podemos hacerlo así: 
+# Sería mejor cambiar los nombres de los documentos (columna "Text"). Por defecto, en ellos se incluye el nombre del archivo. Mientras más metadatos tenga el nombre, más largos serán. Lo que haremos será reescribir esa variable
 
 doc_id <- paste(programas$candidato, programas$eleccion, sep = "-") 
 # es decir, creamos un objeto que pegue candidato y año, separados por un guion -
---
+++ 2018-11_RLadiesSCL_web-scraping.md
@@ -0,0 +1,52 @@
+# Taller sobre web scraping
+
+
+Como no tenemos posibilidad de ofrecer acceso a internet para este taller, es necesario que descargues los paquetes y los datos que utilizaremos con antelación. En caso de que estudies/trabajes en una institución que tenga red "eduroam", entonces no deberías tener problemas para conectarte.
+
+
+## Paquetes necesarios
+
+``` r
+install.packages("tidyverse")
+install.packages("rvest")
+install.packages("janitor")
+``` 
+
+## Guardar los datos en tu computador
+
+El único momento en que utilizaremos internet en el taller es para leer la información del html de las páginas que "escrapearemos". Si crees que no podrás conectarte, entonces es necesario que guardes esa información como un archivo local en tu computador. Para ello, sigue los siguientes pasos.
+
+### 1. Crea un proyecto nuevo
+
+En el menú File > New Project > New Directory. Crea la carpeta en un lugar que te sea fácil encontrar durante el taller. 
+
+### 2. Lee y guarda localmente los datos
+
+Una vez que el proyecto esté creado, crea un nuevo _script_: File > New File > R Script. Luego, ejecuta el siguiente código. Requiere que ya esté instalado el paquete `rvest`.
+
+```r
+library(rvest)
+
+## EJERCICIO 1: asesorías externas diputados
+
+# leer los datos:
+
+asesorias_html <- read_html("https://www.camara.cl/camara/transparencia_asesorias.aspx") 
+
+#guardar los datos en un archivo:
+
+write_html(asesorias_html, "asesorias")
+
+## EJERCICIO 2: reuniones lobby senadores
+
+# leer los datos:
+
+reuniones_html <- read_html("http://www.senado.cl/appsenado/index.php?mo=lobby&ac=GetReuniones")
+
+#guardar los datos en un archivo
+
+write_html(asesorias_html, "reuniones")
+```
+
+Listo.  
+¡Nos vemos en el taller!
